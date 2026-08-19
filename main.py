73d07c8e5615935d422af71558fe7db996cbd3b4
janfilipi222 <janfilipi222@gmail.com>
2026-08-19 18:46:56 +0200

    created a .exe file

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 from pathlib import Path
 
-from PySide6.QtCore import QStandardPaths
 
+from PySide6.QtCore import QStandardPaths
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 
 from presentation.main_window.main_window import MainWindow
 from presentation.main_window.main_view_model import MainViewModel
@@ -12,6 +13,13 @@ from infrastructure.repositories.json_profile_repository import (
     JsonProfileRepository,
 )
 
+def resource_path(relative_path: str) -> Path:
+    if getattr(sys, "frozen", False):
+        base_path = Path(sys._MEIPASS)
+    else:
+        base_path = Path(__file__).resolve().parents[2]
+
+    return base_path / relative_path
 
 
 def main():
@@ -20,6 +28,8 @@ def main():
     app.setOrganizationName("CGM")
     app.setApplicationName("MacroKeypadBackgroundApp")
 
+    
+    app.setWindowIcon(QIcon(f"{resource_path("assets/icon.png")}"))
     
     app_data = Path(QStandardPaths.writableLocation(QStandardPaths.AppLocalDataLocation))
     profile_path = app_data / "profiles"
